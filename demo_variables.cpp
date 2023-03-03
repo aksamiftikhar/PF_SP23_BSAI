f881e2beff571f7e29c7a3e1262b94af4e741e67
M. Aksam Iftikhar <aksam.iftikhar@gmail.com>
2023-03-03 11:14:52 +0500

    Add files via upload

---
+++ demo_variables.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    num1 = 10;
+    num2 = 50;
+
+    cout<<"Numbers are x1 = "<<num1<<", x2 = "<<num2;
+    int res1, res2, res3, res4;
+    res1 = num1 + num2;
+    res2 = num1 - num2;
+    res3 = num1 * num2;
+    res4 = num2 / num1;
+
+
+    /*cout<<res1<<endl;
+    cout<<res2<<endl;
+    cout<<res3<<endl;
+    cout<<res4<<endl;*/
+
+}
